e894d63cf995b0f3d1eccfe5c24e3acb3b5d2f61
goggles320 <goggles320@hotmail.com>
2020-03-07 23:35:11 +1100

    Working Code
    When code is running and generating of password is attempted second time, the 2nd generated password is concatenated onto the previously generated password. 
    Unable to clear previously generated password.

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var generateBtn = document.querySelector("#generate");
 var dispText = document.querySelector("textarea");
 var content = "";
 var length = "";
-var arrayLow = [];
+var arrayLow = ["97","98","99","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120","121","122"];
+var arrayUp = ["65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90"];
 var arrayNum = ["48","49","50","51","52","53","54","55","56","57"];
+var arraySp = ["33","35","36","37","38","40","41","42","43","45","46","58","59","61","63","64","91","93","94","95","96","126"];
 var arrayFinal = [];
 
 //-------------------------------------------------------------------------------------------------------------
@@ -22,10 +24,23 @@ function userInput() {
   var inputLength = prompt("Please select password length - 8 characters minimum)");
   length = parseInt(inputLength);
   //Conditions from user
-  //var lowerCase = confirm("Use Lowercase in password?");
-  //var upperCase = confirm("Use Uppercase in password?");
-  //var numbers = confirm("Use Numbers in password?");
-  //var spChar = confirm("Use special characters in password?");
+  var lowerCase = confirm("Use Lowercase in password?");
+  if (lowerCase){
+    arrayFinal = arrayLow;
+  }
+  //debugger;
+  var upperCase = confirm("Use Uppercase in password?");
+  if (upperCase){
+    arrayFinal = arrayFinal.concat(arrayUp);
+  }
+  var numbers = confirm("Use Numbers in password?");
+  if (numbers){
+    arrayFinal = arrayFinal.concat(arrayNum);
+  }
+  var spChar = confirm("Use special characters in password?");
+  if (spChar){
+    arrayFinal = arrayFinal.concat(arraySp);
+  }
 }
 
 // Generate random password
@@ -34,18 +49,18 @@ function generatePassword(){
   for (var i=0; i < length; i++) {
 
     //Set the parameter based on what user requires in terms of upper/lower case, special characters, etc.
-    key = parseInt(arrayNum[Math.floor(Math.random() * arrayNum.length)]);
+    key = parseInt(arrayFinal[Math.floor(Math.random() * arrayFinal.length)]);
     console.log(key);
         
     //Converts coressponding ASCII number into printed character
     var res = String.fromCharCode(key);
-    debugger;
+    //debugger;
     //Add new character into existing character block
     newContent = content + res;
-    console.log(newContent);
-    debugger;
+    //console.log(newContent);
+    //debugger;
     //Update randomized password onto screen
-    //dispText.textContent = content;
+    //dispText.textContent = newContent;
     dispText.append(newContent);
 
   }  
@@ -54,15 +69,13 @@ function generatePassword(){
 
 // Write password to the #password input
 function writePassword() {
+  newContent = "";
   content = "";
   userInput();
   
   //var password = generatePassword();
   generatePassword();
-  //var passwordText = document.querySelector("#password");
-  //passwordText.textContent = password;
-  //document.write(passwordText);
-  //dispText.append("password");
+  
 }
 
 
